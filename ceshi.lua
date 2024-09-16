ab2716f9fdc28bdb8c746dfc711137d7147dea6d
chenyabe <2037698223@qq.com>
2024-09-16 08:58:12 +0800

    更新 ceshi.lua

--- ceshi.lua
+++ ceshi.lua
@@ -1,49 +1,72 @@
-local RainbowLib =
-loadstring(game:HelpGet("https://raw.githubusercontent.com/YJY2022hh666/yjy/main/rainbow.ui.main.lua?token=GHSAT0AAAAAACESL5MYP6BL6PJ45CVEZRQEZFYXXUA",true))()
-local window = library:new("我爱缝合")
+local OrionLib = loadstring(game:HttpGet('https://pastebin.com/raw/xLRUSiKx'))()
+local Window = OrionLib:MakeWindow({Name = "我爱缝合", HidePremium = false, SaveConfig = true,IntroText = "缝合,启动", ConfigFolder = "😈"})
+local Tab = Window:MakeTab({
+    Name = "信息",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
 
-local creds = window:Tab("信息",'6035145364')
+Tab:AddButton(
+	Name = ("此脚本为缝合脚本"))
+Tab:AddButton(
+	Name = ("致敬传奇缝合王kr X"))
+Tab:AddButton(
+	Name = ("你的用户名为",game.Players.LocalPlayer.Character.Name))
+Tab:AddButton(
+Name = ("你的注入器为",identifyexecutor))
+local Tab = Window:MakeTab({
+    Name = "通用",game.Players.LocalPlayer
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+Tab:AddButton({
+	Name = "飞行",
+	Callback = function()
+loadstring("\108\111\97\100\115\116\114\105\110\103\40\103\97\109\101\58\72\116\116\112\71\101\116\40\34\104\116\116\112\115\58\47\47\112\97\115\116\101\98\105\110\46\99\111\109\47\114\97\119\47\90\66\122\99\84\109\49\102\34\41\41\40\41\10")()
+end
+})    
+Tab:AddButton({
+	Name = "飞车",
+	Callback = function()
+https://raw.githubusercontent.com/dixjzj67/jiaoben/main/vfly.txt
+end
+})
+local LBLG = Instance.new("ScreenGui", getParent)
+local LBL = Instance.new("TextLabel", getParent)
+local player = game.Players.LocalPlayer
 
-local bin = creds:section("信息",true)
+LBLG.Name = "LBLG"
+LBLG.Parent = game.CoreGui
+LBLG.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+LBLG.Enabled = true
+LBL.Name = "LBL"
+LBL.Parent = LBLG
+LBL.BackgroundColor3 = Color3.new(1, 1, 1)
+LBL.BackgroundTransparency = 1
+LBL.BorderColor3 = Color3.new(0, 0, 0)
+LBL.Position = UDim2.new(0.75,0,0.010,0)
+LBL.Size = UDim2.new(0, 133, 0, 30)
+LBL.Font = Enum.Font.GothamSemibold
+LBL.Text = "TextLabel"
+LBL.TextColor3 = Color3.new(1, 1, 1)
+LBL.TextScaled = true
+LBL.TextSize = 14
+LBL.TextWrapped = true
+LBL.Visible = true
 
-    bin:Label("此脚本为缝合")
-    bin:Label("致敬传奇缝合王kr X")
-    bin:Label("你的用户名："..game.Players.LocalPlayer.Character.Name)
-    bin:Label("你的名字："..game.Players.LocalPlayer.Name)
-    bin:Label("你的注入器:"..identifyexecutor())
+local FpsLabel = LBL
+local Heartbeat = game:GetService("RunService").Heartbeat
+local LastIteration, Start
+local FrameUpdateTable = { }
 
-local credits = creds:section("UI设置",true)
-
-    credits:Button("摧毁UI",function()
-        game:GetService("CoreGui")["frosty"]:Destroy()
-    end)
-
-    credits:Toggle("彩虹UI", "", false, function(state)
-        if state then
-        game:GetService("CoreGui")["frosty"].Main.Style = "DropShadow"
-        else
-            game:GetService("CoreGui")["frosty"].Main.Style = "Custom"
-        end
-    end)
-
-credits:Toggle("玩家加入游戏提示", "", false, function(state)
-        if state then
-    
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/boyscp/scriscriptsc/main/bbn.lua"))()
-    end)
-
-local creds = window:Tab("通用",'6035145364')
-
-local bin = creds:section("通用",true)
-
-local credits = creds:section("通用",true)
-
-    credits:Button("飞行",function()
-	loadstring("\108\111\97\100\115\116\114\105\110\103\40\103\97\109\101\58\72\116\116\112\71\101\116\40\34\104\116\116\112\115\58\47\47\112\97\115\116\101\98\105\110\46\99\111\109\47\114\97\119\47\90\66\122\99\84\109\49\102\34\41\41\40\41\10")()
-    end)
-
-credits:Toggle("没做好", "", false, function(state)--这个是一种功能，可以随意开关
-        if state then
-    空一行
-    脚本
-    end)
+local function HeartbeatUpdate()
+	LastIteration = tick()
+	for Index = #FrameUpdateTable, 1, -1 do
+		FrameUpdateTable[Index + 1] = (FrameUpdateTable[Index] >= LastIteration - 1) and FrameUpdateTable[Index] or nil
+	end
+	FrameUpdateTable[1] = LastIteration
+	local CurrentFPS = (tick() - Start >= 1 and #FrameUpdateTable) or (#FrameUpdateTable / (tick() - Start))
+	CurrentFPS = CurrentFPS - CurrentFPS % 1
+	FpsLabel.Text = ("北京时间:"..os.date("%H").."时"..os.date("%M").."分"..os.date("%S"))
+end
+Start = tick()
