e3893f78bffe41a849af5bd7a06cad570cccb23f
chenyabe <2037698223@qq.com>
2024-09-16 08:19:12 +0800

    更新 ceshi.lua

--- ceshi.lua
+++ ceshi.lua
@@ -1,55 +1,65 @@
-local LBLG = Instance.new("ScreenGui", getParent)
-local LBL = Instance.new("TextLabel", getParent)
-local player = game.Players.LocalPlayer
+local OrionLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/atoyayaya/ui/main/UI"))()
+local Window = OrionLib:MakeWindow({Name = "我爱缝合", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+local plrs = game:GetService("Players")
+local plr = plrs.LocalPlayer
+local TeleportService = game:GetService('TeleportService')
+local Gui = Instance.new("ScreenGui")
+local Message = game:GetService("StarterGui")
+local TweenService = game:GetService('TweenService')
+local CoreGui = game:GetService('CoreGui')
 
-LBLG.Name = "LBLG"
-LBLG.Parent = game.CoreGui
-LBLG.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-LBLG.Enabled = true
-LBL.Name = "LBL"
-LBL.Parent = LBLG
-LBL.BackgroundColor3 = Color3.new(1, 1, 1)
-LBL.BackgroundTransparency = 1
-LBL.BorderColor3 = Color3.new(0, 0, 0)
-LBL.Position = UDim2.new(0.75,0,0.010,0)
-LBL.Size = UDim2.new(0, 133, 0, 30)
-LBL.Font = Enum.Font.GothamSemibold
-LBL.Text = "TextLabel"
-LBL.TextColor3 = Color3.new(1, 1, 1)
-LBL.TextScaled = true
-LBL.TextSize = 14
-LBL.TextWrapped = true
-LBL.Visible = true
+_G.autokill = true
 
-local FpsLabel = LBL
-local Heartbeat = game:GetService("RunService").Heartbeat
-local LastIteration, Start
-local FrameUpdateTable = { }
+function autokill()
+	while _G.autokill == true do
+			for i, player in ipairs(game.Players:GetChildren()) do
+			if player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
+			player.Character.HumanoidRootPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame + game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame.lookVector * 1
+			end
+			end
+wait(0.1)
 
-local function HeartbeatUpdate()
-    LastIteration = tick()
-    for Index = #FrameUpdateTable, 1, -1 do
-        FrameUpdateTable[Index + 1] = (FrameUpdateTable[Index] >= LastIteration - 1) and FrameUpdateTable[Index] or nil
-    end
-    FrameUpdateTable[1] = LastIteration
-    local CurrentFPS = (tick() - Start >= 1 and #FrameUpdateTable) or (#FrameUpdateTable / (tick() - Start))
-    CurrentFPS = CurrentFPS - CurrentFPS % 1
-    FpsLabel.Text = ("美国时间:"..os.date("%H").."时"..os.date("%M").."分"..os.date("%S"))
+	end
 end
-Start = tick()
-Heartbeat:Connect(HeartbeatUpdate)
 
-local ui = loadstring(game:HttpGet("https://pastebin.com/raw/Ey3CpCHS"))();        
-local win = ui:new("我爱缝合")
---
-local UITab1 = win:Tab("『信息』",'6035145364')
+_G.autoweight = true
 
-local about = UITab1:section("『信息』",true)
+function autoweight()
+	while _G.autoweight == true do
+			 local part = Instance.new('Part', workspace)
+part.Size = Vector3.new(500, 20, 530.1)
+part.Position = Vector3.new(0, 1000, 133.15)
+part.CanCollide = true
+part.Anchored = true
 
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = part.CFrame + Vector3.new(0, 15, 0)
 
-about:Label("由某源码改编")
-about:Label("作者：chen")
-about:Label("正在优化中")
-about:Label("你的注入器:"..identifyexecutor())
-about:Label("你的名字:"..game.Players.LocalPlayer.Name)
-about:Label("你的用户名:"..game.Players.LocalPlayer.Character.Name)
+local a = true
+local rs = game:GetService("RunService").RenderStepped
+if a == true then
+while rs:wait() do
+for i,v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
+if v.ClassName == "Tool" and v.Name == "Weight" then
+v.Parent = game.Players.LocalPlayer.Character
+end
+end
+game:GetService("Players").LocalPlayer.muscleEvent:FireServer("rep")
+end
+end
+
+	end
+end
+
+_G.autor = true
+
+local Tab = Window:MakeTab({
+	Name = "信息",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+local Section = Tab:AddSection({
+	Name = "信息"
+})
+
+Tab:AddParagraph:("致敬传奇缝合员", "kr X")
