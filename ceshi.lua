7fb13e7efbbfea89d86ffdcc5ae494048da47278
chenyabe <2037698223@qq.com>
2024-09-20 19:12:46 +0800

    更新 ceshi.lua

--- ceshi.lua
+++ ceshi.lua
@@ -1,17 +1,18 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
-local Window = OrionLib:MakeWindow({Name = "大玉脚本", HidePremium = false, SaveConfig = true, ConfigFolder = "大玉脚本",IntroText ＝ "大玉脚本"})
+local Window = OrionLib:MakeWindow({Name = "大玉脚本🤓", HidePremium = false, SaveConfig = true, ConfigFolder = "致敬传奇缝合王krx",IntroText ＝ "大玉脚本🤓"})
 
 local Tab = Window:MakeTab({
-	Name = "通用",
+	Name = "信息",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
 })
 
 local Section = Tab:AddSection({
-	Name = "飞行"
+	Name = "你的注入器："..identifyexecutor
 })
+local Section = Tab:AddSection({
+	Name = "你的用户名："..game.Players.LocalPlayer.Character.Name})
 
---[[
-Name = <string> - The name of the section.
-]]
+
+OrionLib:Destroy()
