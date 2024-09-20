0bb94c049fcb001af3eda122f4b2980483541a51
chenyabe <2037698223@qq.com>
2024-09-20 17:38:03 +0800

    更新 ceshi.lua

--- ceshi.lua
+++ ceshi.lua
@@ -1,5 +1,5 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "大玉脚本🤓", HidePremium = false, SaveConfig = true, ConfigFolder = "大玉脚本",IntroText ＝ "缝合王"})
+local Window = OrionLib:MakeWindow({Name = "大玉脚本🤓", HidePremium = false, SaveConfig = true, ConfigFolder = "大玉脚本🤓",IntroText ＝ "缝合王"})
 
 local Tab = Window:MakeTab({
 	Name = "通用",
